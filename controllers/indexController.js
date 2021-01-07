473658e2fd43bc85bb2a12a63402c12789707d67
pawelswierczynski <pawelswierczynski@pro.wp.pl>
2021-01-07 11:44:16 +0100

    Add a database connection pool

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,7 +1,11 @@
 "use strict";
 
+var database = require("../database.js");
+
 var indexController = {
     renderHomePage(req, res) {
+        database.query("SELECT 2*2;").then(exampleQueryResults => console.log(exampleQueryResults));
+
         res.render("homePage.ejs");
     }
 }
