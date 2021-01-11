93bfc3a7b93c82f0ac77421a921fb2e37d324def
pawelswierczynski <pawelswierczynski@pro.wp.pl>
2021-01-11 18:04:34 +0100

    Fix adding child categories

--- controllers/homePageController.js
+++ controllers/homePageController.js
@@ -1,38 +1,26 @@
 "use strict";
 
 var languages = require("../languages.json");
-//To remove
-var example = require("../models/example");
 var category = require("../models/category");
 
 var homePageController = {
     retrieveHomePage(req, res) {
-        /*example.getExample(example => {
-            console.log(example);
-
-            res.render("homePage.ejs", { language: languages[req.session.language] });
-        });*/
-
-        //res.render("homePage.ejs");
         category.getMainCategories(mainCategories => {
-            
             var polishCategories = [];
-            var englishCategories = [];            
+            var englishCategories = [];  
+
             mainCategories.forEach(mainCategory => {
-                category.getChildCategories(mainCategory.IdKategoria, childCategories => {
-                    mainCategory.KategoriePodrzedne = childCategories;
-                });
-                if(mainCategory.IdJęzyk == 1)
+                if (mainCategory.IdJęzyk == 1)
                 {
                     polishCategories.push(mainCategory);
                 }
-                else if(mainCategory.IdJęzyk == 2)
+                else if (mainCategory.IdJęzyk == 2)
                 {
                     englishCategories.push(mainCategory);
                 }
             });
-            console.log(mainCategories);
-            if(req.session.language == "pl")
+            
+            if (req.session.language == "pl")
             {
                 res.render("homePage.ejs", { language: languages[req.session.language], mainCategories:polishCategories });
             }
@@ -40,10 +28,8 @@ var homePageController = {
             {
                 res.render("homePage.ejs", { language: languages[req.session.language], mainCategories:englishCategories });
             }
-            
         });
     }
-    
 }
 
 module.exports = homePageController;
--- models/category.js
+++ models/category.js
@@ -4,16 +4,30 @@ var database = require("../database");
 
 function aggregateMainCategories(mainCategories){
     var aggregatedMainCategories = [];
-    mainCategories.forEach(mainCategory => {
-        if(aggregatedMainCategories.find(searchCategory => searchCategory.IdKategoria === mainCategory.IdKategoria)){
 
+    mainCategories.forEach(mainCategory => {
+        if (!aggregatedMainCategories.find(searchCategory => searchCategory.IdKategoria === mainCategory.IdKategoria)){
+            aggregatedMainCategories.push({
+                IdKategoria: mainCategory.IdKategoria,
+                Nazwa: mainCategory.Nazwa,
+                IdJęzyk: mainCategory.IdJęzyk,
+                KategoriePodrzedne: []
+            });
         }
-        else{
-            aggregatedMainCategories.push({IdKategoria:mainCategory.IdKategoria, Nazwa:mainCategory.Nazwa, IdJęzyk:mainCategory.IdJęzyk, KategoriePodrzędne:[]})
+
+        if (mainCategory.IdKatNadrzędnej2 != null) {
+            const mainCategoryIndex = aggregatedMainCategories.findIndex(searchCategory => searchCategory.IdKategoria === mainCategory.IdKatNadrzędnej2);
+
+            aggregatedMainCategories[mainCategoryIndex]["KategoriePodrzedne"].push({
+                IdKategoriaPodrzędna: mainCategory.IdKategoriaPodrzędna,
+                NazwaPodrzędnej: mainCategory.NazwaPodrzędnej,
+                DataUtworzeniaPodrzędnej: mainCategory.DataUtworzeniaPodrzędnej,
+                IdJęzykPodrzędnej: mainCategory.IdJęzykPodrzędnej
+            });
         }
-        const mainCategoryIndex = aggregatedMainCategories.findIndex(searchCategory => searchCategory.IdKategoria === mainCategory.IdKatNadrzędnej2);
-        aggregatedMainCategories[mainCategoryIndex].KategoriePodrzędne.push({IdKategoriaPodrzędna:mainCategory.IdKategoriaPodrzędna, NazwaPodrzędnej:mainCategory.NazwaPodrzędnej, DataUtworzeniaPodrzędnej:mainCategory.DataUtworzeniaPodrzędnej, IdJęzykPodrzędnej:mainCategory.IdJęzykPodrzędnej})
     });
+
+    return aggregatedMainCategories;
 }
 
 module.exports = {
@@ -21,10 +35,5 @@ module.exports = {
         database.query("SELECT * FROM Kategoria as K1 LEFT JOIN (SELECT IdKategoria as IdKategoriaPodrzędna, Nazwa as NazwaPodrzędnej, DataUtworzenia as DataUtworzeniaPodrzędnej, IdJęzyk as IdJęzykPodrzędnej, IdKatNadrzędnej as IdKatNadrzędnej2 FROM Kategoria) as K2 ON K1.IdKategoria = K2.IdKatNadrzędnej2 WHERE K1.IdKatNadrzędnej IS NULL").then(mainCategories =>{
             callback(aggregateMainCategories(mainCategories));
         });                 
-    },
-    getChildCategories(parentCategoryId, callback){
-        database.query("SELECT * FROM Kategoria WHERE IdKatNadrzędnej = " + parentCategoryId).then(childCategories =>{
-            callback(childCategories);
-        });
     }
 };
