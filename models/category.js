5282739ee90c06e377b46ee81dd521af9b26e3c7
SzczepaniakJ <szczepan.szczepan2@gmail.com>
2021-01-11 17:47:01 +0100

    kategorie podrzedne

--- models/category.js
+++ models/category.js
@@ -2,10 +2,24 @@
 
 var database = require("../database");
 
+function aggregateMainCategories(mainCategories){
+    var aggregatedMainCategories = [];
+    mainCategories.forEach(mainCategory => {
+        if(aggregatedMainCategories.find(searchCategory => searchCategory.IdKategoria === mainCategory.IdKategoria)){
+
+        }
+        else{
+            aggregatedMainCategories.push({IdKategoria:mainCategory.IdKategoria, Nazwa:mainCategory.Nazwa, IdJęzyk:mainCategory.IdJęzyk, KategoriePodrzędne:[]})
+        }
+        const mainCategoryIndex = aggregatedMainCategories.findIndex(searchCategory => searchCategory.IdKategoria === mainCategory.IdKatNadrzędnej2);
+        aggregatedMainCategories[mainCategoryIndex].KategoriePodrzędne.push({IdKategoriaPodrzędna:mainCategory.IdKategoriaPodrzędna, NazwaPodrzędnej:mainCategory.NazwaPodrzędnej, DataUtworzeniaPodrzędnej:mainCategory.DataUtworzeniaPodrzędnej, IdJęzykPodrzędnej:mainCategory.IdJęzykPodrzędnej})
+    });
+}
+
 module.exports = {
     getMainCategories(callback){
-        database.query("SELECT * FROM Kategoria as K1 LEFT JOIN Kategoria as K2 ON K1.IdKategoria = K2.IdKatNadrzędnej WHERE K1.IdKatNadrzędnej IS NULL;").then(mainCategories =>{
-            callback(mainCategories);
+        database.query("SELECT * FROM Kategoria as K1 LEFT JOIN (SELECT IdKategoria as IdKategoriaPodrzędna, Nazwa as NazwaPodrzędnej, DataUtworzenia as DataUtworzeniaPodrzędnej, IdJęzyk as IdJęzykPodrzędnej, IdKatNadrzędnej as IdKatNadrzędnej2 FROM Kategoria) as K2 ON K1.IdKategoria = K2.IdKatNadrzędnej2 WHERE K1.IdKatNadrzędnej IS NULL").then(mainCategories =>{
+            callback(aggregateMainCategories(mainCategories));
         });                 
     },
     getChildCategories(parentCategoryId, callback){
