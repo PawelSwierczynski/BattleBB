61ce0631c63a70b137b2895e8e70a29736b71709
SzczepaniakJ <szczepan.szczepan2@gmail.com>
2021-01-14 17:40:48 +0100

    thread count

--- controllers/subforumController.js
+++ controllers/subforumController.js
@@ -5,11 +5,12 @@ var subforum = require("../models/subforum")
 
 var subforumController = {
     retrieveSubforum(req, res) {
-        subforum.getSubforums(req.query.identifier, (subforums, parentCategoryName)=> {
+        subforum.getSubforums(req.query.identifier, (subforums, parentCategoryName, threads)=> {
             res.render("categoryPage.ejs", {
                 language: languages[req.session.language],
                 subforums: subforums,
                 parentCategoryName: parentCategoryName,
+                threads: threads,
                 lastVisitedUrl: req.path
             });
         });
--- models/subforum.js
+++ models/subforum.js
@@ -4,6 +4,8 @@ var database = require("../database");
 
 var dateFormatter = require("../utilities/dateFormatter");
 
+var subforumsThreads = [];
+
 function formatSubforumsDates(subforums) {
     subforums.forEach(subforum => {
         subforum["DataUtworzenia"] = dateFormatter.formatDate(subforum["DataUtworzenia"])
@@ -11,14 +13,33 @@ function formatSubforumsDates(subforums) {
     return subforums;
 }
 
+function getNumberOfThreads(threads, subforums) {
+    subforums.forEach(subforum => {
+        var help = 0;
+        threads.forEach(thread =>{
+            if(thread.IdSubforum === subforum.IdSubforum)
+            {
+                help++;
+            }
+        });
+        subforumsThreads.push({
+            IdSubforum: subforum.IdSubforum,
+            ThreadCount: help
+        });
+    });
+    return subforumsThreads;    
+}
+
+
 
 module.exports = {
     getSubforums(parentCategoryIdentifier, callback){
         database.query("SELECT * FROM Subforum Where IdKategoria = "  + parentCategoryIdentifier).then(subforums =>{
-            database.query("SELECT Nazwa FROM Kategoria Where IdKategoria = "  + parentCategoryIdentifier).then(parentCategoryName =>{
-                callback(formatSubforumsDates(subforums), parentCategoryName);
-            }); 
-            
+            database.query("SELECT Nazwa FROM Kategoria Where IdKategoria = "  + parentCategoryIdentifier).then(parentCategoryName =>{                
+                    database.query("SELECT * FROM Wątek as w JOIN Subforum as s ON w.IdSubforum = s.IdSubforum WHERE s.IdKategoria = " + parentCategoryIdentifier).then(threads =>{                 
+                        callback(formatSubforumsDates(subforums), parentCategoryName, getNumberOfThreads(threads, subforums));
+                    });                        
+            });             
         });                 
     }
 };
