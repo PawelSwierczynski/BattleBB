a94b1f995c1eee708e6d7395c131144792a22622
pawelswierczynski <pawelswierczynski@pro.wp.pl>
2021-01-14 12:38:56 +0100

    Add a thread's title and number of pages

--- controllers/threadController.js
+++ controllers/threadController.js
@@ -1,18 +1,25 @@
 "use strict";
 
-const postsPerPageCount = 20;
+const config = require("../config.json");
 
 var languages = require("../languages.json");
 var post = require("../models/post");
 
+function calculateNumberOfPages(postCount) {
+    return Math.ceil(postCount / config.postsPerPage);
+}
+
 var threadController = {
     retrievePostsLatestVersions(req, res) {
-        post.getPostsLatestVersions(req.query.identifier, req.query.page, postsPerPageCount, postsLatestVersions => {
-            console.log(postsLatestVersions);
-
+        post.getPostsLatestVersions(req.query.identifier, req.query.page, config.postsPerPage, (thread, postsLatestVersions) => {
+            const numberOfPages = calculateNumberOfPages(thread.PostCount);
+            
             res.render("thread.ejs", {
                 language: languages[req.session.language],
+                thread: thread,
                 posts: postsLatestVersions,
+                currentPage: req.query.page,
+                numberOfPages: numberOfPages,
                 lastVisitedUrl: req.originalUrl
             });
         });
