abc78ba62a5835653338dbec84728169a5ad2d7d
SzczepaniakJ <szczepan.szczepan2@gmail.com>
2021-02-08 17:05:46 +0100

    blocked admin and moderator panels

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -5,74 +5,110 @@ var admin = require("../models/admin");
 var messageHandler = require("../utilities/messageHandler");
 
 var adminController = {
-    retreiveCategories(req, res) {    
-        admin.retreiveCategories(categories => {
-            res.render("addCategoryPage.ejs", {
-                language: languages[req.session.language],
-                lastVisitedUrl: req.originalUrl,
-                isLoggedIn: req.session.isLoggedIn,
-                userRole: req.session.userRole,
-                categories: categories,
-                errorMessage: messageHandler.retrieveErrorMessage(req),
-                noticeMessage: messageHandler.retrieveNoticeMessage(req)
-            });
-        });    
+    retreiveCategories(req, res) {   
+        if (req.session.userRole === 1) {     
+            admin.retreiveCategories(categories => {
+                res.render("addCategoryPage.ejs", {
+                    language: languages[req.session.language],
+                    lastVisitedUrl: req.originalUrl,
+                    isLoggedIn: req.session.isLoggedIn,
+                    userRole: req.session.userRole,
+                    categories: categories,
+                    errorMessage: messageHandler.retrieveErrorMessage(req),
+                    noticeMessage: messageHandler.retrieveNoticeMessage(req)
+                });
+            });  
+        }
+        else{
+            messageHandler.setErrorMessage(req, "adminRoleRequired");
+            res.redirect("/");
+        } 
+          
             
     },
-    retreiveSubforum(req, res) {    
-        admin.retreiveCategories(categories => {
-            res.render("addSubforumPage.ejs", {
-                language: languages[req.session.language],
-                lastVisitedUrl: req.originalUrl,
-                isLoggedIn: req.session.isLoggedIn,
-                userRole: req.session.userRole,
-                categories: categories,
-                errorMessage: messageHandler.retrieveErrorMessage(req),
-                noticeMessage: messageHandler.retrieveNoticeMessage(req)
-            });
-        });    
+    retreiveSubforum(req, res) {   
+        if (req.session.userRole === 1) {     
+            admin.retreiveCategories(categories => {
+                res.render("addSubforumPage.ejs", {
+                    language: languages[req.session.language],
+                    lastVisitedUrl: req.originalUrl,
+                    isLoggedIn: req.session.isLoggedIn,
+                    userRole: req.session.userRole,
+                    categories: categories,
+                    errorMessage: messageHandler.retrieveErrorMessage(req),
+                    noticeMessage: messageHandler.retrieveNoticeMessage(req)
+                });
+            });  
+        }
+        else{
+            messageHandler.setErrorMessage(req, "adminRoleRequired");
+            res.redirect("/");
+        } 
+          
             
     },
-    retreiveUsers(req, res) {   
-        admin.retreiveUsers(users => { 
-            res.render("changeUserRolePage.ejs", {
-                language: languages[req.session.language],
-                lastVisitedUrl: req.originalUrl,
-                isLoggedIn: req.session.isLoggedIn,
-                userRole: req.session.userRole,
-                users: users,
-                errorMessage: messageHandler.retrieveErrorMessage(req),
-                noticeMessage: messageHandler.retrieveNoticeMessage(req)
-            });    
-        });  
+    retreiveUsers(req, res) {  
+        if (req.session.userRole === 1) {     
+            admin.retreiveUsers(users => { 
+                res.render("changeUserRolePage.ejs", {
+                    language: languages[req.session.language],
+                    lastVisitedUrl: req.originalUrl,
+                    isLoggedIn: req.session.isLoggedIn,
+                    userRole: req.session.userRole,
+                    users: users,
+                    errorMessage: messageHandler.retrieveErrorMessage(req),
+                    noticeMessage: messageHandler.retrieveNoticeMessage(req)
+                });    
+            }); 
+        }
+        else{
+            messageHandler.setErrorMessage(req, "adminRoleRequired");
+            res.redirect("/");
+        } 
+         
     },
-    addCategory(req, res) {       
-        admin.createNewCategory(req.body.categoryName, req.body.language, req.body.parentCategory, text => {
-            res.render("adminPage.ejs", {
-                language: languages[req.session.language],
-                lastVisitedUrl: req.originalUrl,
-                isLoggedIn: req.session.isLoggedIn,
-                userRole: req.session.userRole,
-                text: text,
-                errorMessage: messageHandler.retrieveErrorMessage(req),
-                noticeMessage: messageHandler.retrieveNoticeMessage(req)
+    addCategory(req, res) {      
+        if (req.session.userRole === 1) {     
+            admin.createNewCategory(req.body.categoryName, req.body.language, req.body.parentCategory, text => {
+                res.render("adminPage.ejs", {
+                    language: languages[req.session.language],
+                    lastVisitedUrl: req.originalUrl,
+                    isLoggedIn: req.session.isLoggedIn,
+                    userRole: req.session.userRole,
+                    text: text,
+                    errorMessage: messageHandler.retrieveErrorMessage(req),
+                    noticeMessage: messageHandler.retrieveNoticeMessage(req)
+                });
             });
-        });
+        }
+        else{
+            messageHandler.setErrorMessage(req, "adminRoleRequired");
+            res.redirect("/");
+        } 
+        
     },
-    addSubforum(req, res) {       
-        admin.createNewSubforum(req.body.subforumName, req.body.parentCategory, text => {
-            res.render("adminPage.ejs", {
-                language: languages[req.session.language],
-                lastVisitedUrl: req.originalUrl,
-                isLoggedIn: req.session.isLoggedIn,
-                userRole: req.session.userRole,
-                text: text,
-                errorMessage: messageHandler.retrieveErrorMessage(req),
-                noticeMessage: messageHandler.retrieveNoticeMessage(req)
+    addSubforum(req, res) {     
+        if (req.session.userRole === 1) {     
+            admin.createNewSubforum(req.body.subforumName, req.body.parentCategory, text => {
+                res.render("adminPage.ejs", {
+                    language: languages[req.session.language],
+                    lastVisitedUrl: req.originalUrl,
+                    isLoggedIn: req.session.isLoggedIn,
+                    userRole: req.session.userRole,
+                    text: text,
+                    errorMessage: messageHandler.retrieveErrorMessage(req),
+                    noticeMessage: messageHandler.retrieveNoticeMessage(req)
+                });
             });
-        });
+        }
+        else{
+            messageHandler.setErrorMessage(req, "adminRoleRequired");
+            res.redirect("/");
+        }  
+        
     },
     changeUserRole(req, res) {   
+        if (req.session.userRole === 1) {     
             admin.changeUserRole(req.body.username, req.body.userRole, text => {
                 res.render("adminPage.ejs", {
                 language: languages[req.session.language],
@@ -83,9 +119,16 @@ var adminController = {
                 errorMessage: messageHandler.retrieveErrorMessage(req),
                 noticeMessage: messageHandler.retrieveNoticeMessage(req)
             });
-        });  
+        }); 
+        }
+        else{
+            messageHandler.setErrorMessage(req, "adminRoleRequired");
+            res.redirect("/");
+        }
+             
     },
-    retreivePanel(req, res) {        
+    retreivePanel(req, res) {   
+        if (req.session.userRole === 1) {     
         res.render("adminPage.ejs", {
             language: languages[req.session.language],
             lastVisitedUrl: req.originalUrl,
@@ -95,7 +138,13 @@ var adminController = {
             errorMessage: messageHandler.retrieveErrorMessage(req),
             noticeMessage: messageHandler.retrieveNoticeMessage(req)
         });
+    }
+    else{
+        messageHandler.setErrorMessage(req, "adminRoleRequired");
+        res.redirect("/");
+    }
 }
 }
 
 module.exports = adminController;
+
--- controllers/moderatorController.js
+++ controllers/moderatorController.js
@@ -5,7 +5,8 @@ var moderator = require("../models/moderator");
 var messageHandler = require("../utilities/messageHandler");
 
 var moderatorController = {
-    retreiveUsers(req, res) {   
+    retreiveUsers(req, res) {  
+        if (req.session.userRole === 1 || req.session.userRole === 2) { 
         moderator.retreiveUsers(users => { 
             res.render("banUserPage.ejs", {
                 language: languages[req.session.language],
@@ -17,8 +18,15 @@ var moderatorController = {
                 noticeMessage: messageHandler.retrieveNoticeMessage(req)
             });    
         });  
+        }
+        else{
+            messageHandler.setErrorMessage(req, "moderatorRoleRequired");
+
+            res.redirect("/");
+        }
     },
     retreiveReportedPosts(req, res) {   
+        if (req.session.userRole === 1 || req.session.userRole === 2) {
         moderator.retreiveReportedPosts(reportedPosts => { 
             res.render("reportedPostsPage.ejs", {
                 language: languages[req.session.language],
@@ -29,9 +37,16 @@ var moderatorController = {
                 errorMessage: messageHandler.retrieveErrorMessage(req),
                 noticeMessage: messageHandler.retrieveNoticeMessage(req)
             });    
-        });  
+        }); 
+        } 
+        else{
+            messageHandler.setErrorMessage(req, "moderatorRoleRequired");
+
+            res.redirect("/");
+        }
     },
     changeUserRole(req, res) {   
+        if (req.session.userRole === 1 || req.session.userRole === 2) {
             moderator.changeUserRole(req.body.username, req.body.userRole, text => {
                 res.render("moderatorPage.ejs", {
                 language: languages[req.session.language],
@@ -42,10 +57,16 @@ var moderatorController = {
                 errorMessage: messageHandler.retrieveErrorMessage(req),
                 noticeMessage: messageHandler.retrieveNoticeMessage(req)
             });
-        });  
+        }); 
+        }
+        else{
+            messageHandler.setErrorMessage(req, "moderatorRoleRequired");
+
+            res.redirect("/");
+        } 
     },
     deletePostAndBanUser(req, res) {  
-        if (req.session.isLoggedIn) {
+        if (req.session.userRole === 1 || req.session.userRole === 2) {
             moderator.deletePostAndBanUser(req.params.IdPost, req.params.IdUser, (error) => {
                 if (!error) {
                     messageHandler.setErrorMessage(req, "justError");
@@ -58,13 +79,13 @@ var moderatorController = {
             });
         }
         else {
-            messageHandler.setErrorMessage(req, "logInRequired");
+            messageHandler.setErrorMessage(req, "moderatorRoleRequired");
 
-            res.redirect("/user/logIn");
+            res.redirect("/");
         }
     },
     deletePost(req, res) {  
-        if (req.session.isLoggedIn) {
+        if (req.session.userRole === 1 || req.session.userRole === 2) {
             moderator.deletePost(req.params.IdPost, (error) => {
                 if (!error) {
                     messageHandler.setErrorMessage(req, "justError");
@@ -77,13 +98,13 @@ var moderatorController = {
             });
         }
         else {
-            messageHandler.setErrorMessage(req, "logInRequired");
+            messageHandler.setErrorMessage(req, "moderatorRoleRequired");
 
-            res.redirect("/user/logIn");
+            res.redirect("/");
         }
     },
     deleteReport(req, res) {  
-        if (req.session.isLoggedIn) {
+        if (req.session.userRole === 1 || req.session.userRole === 2) {
             moderator.deleteReport(req.params.IdPost, (error) => {
                 if (!error) {
                     messageHandler.setErrorMessage(req, "justError");
@@ -96,13 +117,13 @@ var moderatorController = {
             });
         }
         else {
-            messageHandler.setErrorMessage(req, "logInRequired");
+            messageHandler.setErrorMessage(req, "moderatorRoleRequired");
 
-            res.redirect("/user/logIn");
+            res.redirect("/");
         }
     },
     retreiveReports(req, res) {  
-        if (req.session.isLoggedIn) {
+        if (req.session.userRole === 1 || req.session.userRole === 2) {
             moderator.retreiveReports(req.params.IdPost, (reports) => {
                     res.render("reportsPage.ejs", {
                         language: languages[req.session.language],
@@ -116,21 +137,29 @@ var moderatorController = {
             });
         }
         else {
-            messageHandler.setErrorMessage(req, "logInRequired");
+            messageHandler.setErrorMessage(req, "moderatorRoleRequired");
 
-            res.redirect("/user/logIn");
+            res.redirect("/");
         }
     },
-    retreivePanel(req, res) {        
-        res.render("moderatorPage.ejs", {
-            language: languages[req.session.language],
-            lastVisitedUrl: req.originalUrl,
-            isLoggedIn: req.session.isLoggedIn,
-            userRole: req.session.userRole,
-            text: "",
-            errorMessage: messageHandler.retrieveErrorMessage(req),
-            noticeMessage: messageHandler.retrieveNoticeMessage(req)
-        });
+    retreivePanel(req, res) {      
+        if (req.session.userRole === 1 || req.session.userRole === 2) {
+
+            res.render("moderatorPage.ejs", {
+                language: languages[req.session.language],
+                lastVisitedUrl: req.originalUrl,
+                isLoggedIn: req.session.isLoggedIn,
+                userRole: req.session.userRole,
+                text: "",
+                errorMessage: messageHandler.retrieveErrorMessage(req),
+                noticeMessage: messageHandler.retrieveNoticeMessage(req)
+            });
+        }
+        else{
+            messageHandler.setErrorMessage(req, "moderatorRoleRequired");
+
+            res.redirect("/");
+        }
 }
 }
 
